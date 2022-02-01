50609932c1528e4d63ce25670a1b6085eb9b1c8b
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-02-01 22:18:05 +0000

    Vuex, fetch() and nuxtServerInit()

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -8,30 +8,11 @@ import PostList from "@/components/Posts/PostList";
 
 export default {
   components: { PostList },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "This is our first post!",
-            thumbnail:
-              "https://imageio.forbes.com/specials-images/imageserve/61d52d4e3a76ed81ac034ea8/The-10-Tech-Trends-That-Will-Transform-Our-World/960x0.jpg?fit=bounds&format=jpg&width=960",
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post!",
-            thumbnail:
-              "https://imageio.forbes.com/specials-images/imageserve/61d52d4e3a76ed81ac034ea8/The-10-Tech-Trends-That-Will-Transform-Our-World/960x0.jpg?fit=bounds&format=jpg&width=960",
-          },
-        ],
-      });
-    }, 1500);
-  },
-  created() {
-    this.$store.dispatch("setPosts", this.loadedPosts);
+
+  computed: {
+    loadedPosts() {
+      this.$store.getters.loadedPosts;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -11,6 +11,29 @@ const createStore = () => {
       },
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit("setPosts", [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is our first post!",
+                thumbnail:
+                  "https://imageio.forbes.com/specials-images/imageserve/61d52d4e3a76ed81ac034ea8/The-10-Tech-Trends-That-Will-Transform-Our-World/960x0.jpg?fit=bounds&format=jpg&width=960",
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is our second post!",
+                thumbnail:
+                  "https://imageio.forbes.com/specials-images/imageserve/61d52d4e3a76ed81ac034ea8/The-10-Tech-Trends-That-Will-Transform-Our-World/960x0.jpg?fit=bounds&format=jpg&width=960",
+              },
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
       },
