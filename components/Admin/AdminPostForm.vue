326af4ed667beea47bdfa622cd6b88171b82c3bd
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-02-03 19:46:54 +0000

    Initializing the Store

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -4,12 +4,15 @@
 
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
 
-    <AppControlInput v-model="editedPost.thumbnailLink"
+    <AppControlInput v-model="editedPost.thumbnail"
       >Thumbnail Link</AppControlInput
     >
 
     <AppControlInput control-type="textarea" v-model="editedPost.content"
       >Content</AppControlInput
+    >
+    <AppControlInput control-type="textarea" v-model="editedPost.previewText"
+      >Preview</AppControlInput
     >
 
     <AppButton type="submit">Save</AppButton>
@@ -40,7 +43,13 @@ export default {
     return {
       editedPost: this.post
         ? { ...this.post }
-        : { author: "", title: "", thumbnailLink: "", content: "" },
+        : {
+            author: "",
+            title: "",
+            thumbnail: "",
+            content: "",
+            previewText: "",
+          },
     };
   },
   methods: {
